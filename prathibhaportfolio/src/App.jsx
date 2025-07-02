b70d6795c70865738d0aacae82aaf08a0d737a5e
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-02 15:42:45 +0530

    Add FeatureCards section and update App layout
    Introduced a new FeatureCards section to display skills using data from constants. Updated App.jsx to include FeatureCards in the main layout. Also removed an unused image (project1.png) from the public images directory.

--- prathibhaportfolio/src/App.jsx
+++ prathibhaportfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import TopNavBar from "./components/TopNavBar";
+import FeatureCards from "./sections/FeatureCards";
 import Hero from "./sections/Hero";
 import AppShowcase from "./sections/ShowcaseSection";
 
@@ -6,7 +7,9 @@ const App = () => (
   <>
     <TopNavBar />
     <Hero />
+    <FeatureCards/>
     <AppShowcase/>
+    
   </>
 );
 
